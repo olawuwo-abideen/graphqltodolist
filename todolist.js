6ee8b23ab43d80de507f38c05a5e4a8b7668daab
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-04-30 15:12:14 +0100

    add todolist

---
+++ todolist.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    id: 1,
+    title: 'Read a Book',
+    done: false
+  }, {
+    id: 2,
+    title: 'Fix a bug',
+    done: false
+  }, {
+    id: 3,
+    title: 'Go for a walk',
+    done: false
+  },
+  {
+    id: 4,
+    title: 'Cook a meal',
+    done: false
+  },
+  {
+    id: 5,
+    title: 'Attend team meeting',
+    done: false
+  }
+
+];
